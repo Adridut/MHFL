840c7128e71a49c204ce243471ab96bc7b8d0d29
Adrien Dutfoy <dutfoy.adrien@hotmail.fr>
2023-08-20 19:14:06 +0200

    comparison for NB and SVM works

--- comparison_models.py
+++ comparison_models.py
@@ -5,7 +5,7 @@ from utils import print_log
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 from sklearn.model_selection import train_test_split
-import torch
+from sklearn.metrics import f1_score, accuracy_score
 
 
 
@@ -17,12 +17,10 @@ def train(selected_modalities, label,  model, trials, test_size):
         X, y, _, _, _, _, _, _, _ = load_ASERTAIN(selected_modalities[0], label, 80, 10, 10, i)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
         y_pred = model.fit(X_train, y_train).predict(X_test)
-        evaluator = Evaluator(["accuracy", "f1_score"])
-        y_pred = torch.from_numpy(y_pred)
-        y_test = torch.from_numpy(y_test)
-        res = evaluator.test(y_test, y_pred)
-        acc += (res['accuracy'])
-        f1 += (res['f1_score'])
+        acc += accuracy_score(y_test, y_pred)
+        f1 += f1_score(y_test, y_pred, average='macro')
+
+
     
     print(acc/trials, f1/trials)
 
@@ -30,10 +28,10 @@ def train(selected_modalities, label,  model, trials, test_size):
 if __name__ == "__main__":
 
     #Params
-    selected_modalities = ['ECG', 'GSR', 'EMO', 'EEG']
+    selected_modalities = [['ECG', 'GSR', 'EMO', 'EEG']]
     label = 'valence'
-    # model = svm.SVC()
-    model = GaussianNB()
+    model = svm.SVC()
+    # model = GaussianNB()
     trials = 10
     test_size = 0.2
 
